71b4bd923b8d72d7ddca4fdd1ea8614796cd070f
Cong Trung <42043537+congtrung2k1@users.noreply.github.com>
2020-02-15 14:37:32 +0700

    master

---
+++ batamlinux/crypto/Basic LC4/basic.py
@@ -0,0 +1,11 @@
+import lc4
+
+key = ''
+nonce = 'blug'
+ciphertext = 'fsrp3d5pqn#ne5mbazgkl'
+
+flag = decrypt (key, ciphertext, nonce=nonce, verbose=False)
+#print (flag)
+
+
+#key= qfel_9p4vk75gijy6bzdnwx2ts8roh#aum3c
